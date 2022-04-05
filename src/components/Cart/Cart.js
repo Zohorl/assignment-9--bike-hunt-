e57f02ae71890e70cf4431d64663ee284870a16b
Zohorl <zohorul109561@gmail.com>
2022-04-05 21:24:58 +0600

    readme.md update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cart = ({ rv }) => {
     const { name, rating, review } = rv;
     return (
-        <div className='border-2 mb-3 p-3 rounded bg-green-200'>
+        <div className='border-2 mb-5 p-3 rounded bg-green-200 drop-shadow-xl'>
             <h2 className='text-2xl'>Name : {name}</h2>
             <h3 className='text-xl'>Review : {review}</h3>
             <h3>Rating : <span className='text-xl text-orange-500'> {rating}</span></h3>
--- src/components/Reviews/Review/Review.js
+++ src/components/Reviews/Review/Review.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Review = ({ singleReview }) => {
     const { name, review, rating } = singleReview;
     return (
-        <div className='border-2 m-3 p-3 rounded'>
-            <h2>Name : {name}</h2>
-            <h4>Review : {review}</h4>
-            <h4>Rating : {rating}</h4>
+        <div className='border-2 m-3 p-3 rounded bg-green-200 drop-shadow-xl'>
+            <h2 className='text-2xl'>Name : {name}</h2>
+            <h4 className='text-xl'>Review : {review}</h4>
+            <h4>Rating : <span className='text-xl text-orange-500'> {rating}</span></h4>
         </div>
     );
 };
